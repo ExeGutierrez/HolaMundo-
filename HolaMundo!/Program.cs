750ac5da87a1f984b27c8b3869923fb08b1e8c52
Pablo Exequiel Gutierrez <pablo.e.gutierrez@outlook.com>
2024-04-27 13:25:15 -0300

    Agregar archivos de proyecto.

---
+++ HolaMundo!/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundo_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+
+
+            Console.WriteLine("Presiona cualquier tecla para finalizar");
+            Console.ReadKey();
+
+
+        }
+    }
+}
